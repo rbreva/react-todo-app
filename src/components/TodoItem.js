a7bd2757a29acae29bedb123ec17ff0a81d93693
Mauricio Mendoza <rbreva@gmail.com>
2022-11-01 10:05:34 -0500

    react tutorial

---
+++ src/components/TodoItem.js
@@ -0,0 +1,40 @@
+/* eslint-disable max-len, no-console, react/destructuring-assignment, react/prop-types, react/sort-comp, react/state-in-constructor */
+import React from 'react';
+import styles from './TodoItem.module.css';
+
+function TodoItem(props) {
+  const completedStyle = {
+    fontStyle: 'italic',
+    color: '#595959',
+    opacity: 0.4,
+    textDecoration: 'line-through',
+  };
+
+  const { completed, id, title } = props.todo;
+
+  const handleEditing = () => {
+    console.log('edit mode activated');
+  };
+
+  return (
+    <li className={styles.item}>
+      <div onDoubleClick={handleEditing}>
+        <input
+          type="checkbox"
+          className={styles.checkbox}
+          checked={completed}
+          onChange={() => props.handleChangeProps(id)}
+        />
+        <button type="button" onClick={() => props.deleteTodoProps(id)}>
+          Delete
+        </button>
+        <span style={completed ? completedStyle : null}>
+          {title}
+        </span>
+      </div>
+      <input type="text" className={styles.textInput} />
+    </li>
+  );
+}
+
+export default TodoItem;
